770056aba4f0217948f3c303754b81aaf179dcbb
Alfred-Lau <jian.liu@icloud.com>
2018-03-01 15:20:38 +0800

    [string] add the string structure

---
+++ string.js
@@ -0,0 +1,72 @@
+//TODO: 1.1 字符的Unicode表示法
+
+{
+    // js允许采用\uxxxx形式表示一个字符串
+    console.log('\u0000','- -', '\uffff');
+}
+
+//TODO: 2.1 codePointAt()的使用：
+{
+    
+}
+
+//TODO: 3.1 String.fromCodePoint():
+
+{
+
+}
+
+//TODO: 4.1 字符串的遍历器接口
+{
+    
+}
+
+// TODO: 5.1 at()的使用
+{
+    
+}
+
+// TODO: 6.1 normalize()的使用
+{
+
+}
+
+// TODO: 7.1 includes(), startsWith(), endsWith()
+{
+
+}
+
+// TODO: 8.1 repeat()的使用
+{
+
+}
+
+// TODO: padStart(), padEnd()
+{
+
+}
+
+// TODO: matchAll()的使用
+{
+    
+}
+
+// TODO:模板字符串
+{
+    
+}
+
+//TODO:标签模板
+{
+
+}
+
+// TODO:String.raw()
+{
+
+}
+
+// TODO: 模板字符串的限制
+{
+    
+}
