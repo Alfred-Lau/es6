680ce9f74548508d50a5021067d9e6d9ce789242
Alfred-Lau <jian.liu@icloud.com>
2018-03-01 15:19:41 +0800

    [regex] add the regex structure

---
+++ regex.js
@@ -0,0 +1,49 @@
+// TODO:1.1 RegExp的扩展
+{
+    
+}
+
+// TODO: 2.1 字符串的正则方法
+{
+    
+}
+
+// TODO:3.1 u修饰符
+{
+    
+}
+
+// TODO:4.1 y修饰符
+{
+    
+}
+// TODO: 5.1 sticky属性
+{
+    
+}
+
+// TODO: 6.1 flags属性
+{
+    
+}
+// TODO:7.1 s修饰符：dotAll模式
+{
+    
+}
+
+// TODO:8.1 后行断言
+{
+    
+}
+// TODO:9.1 Unicode属性类
+{ 
+
+}
+// TODO:10.1 具名组匹配
+{
+    
+}
+// TODO: 11.1 String.prototype.matchAll()的使用
+{
+    
+}
