61ceef7b8f6289290834ce158a1d9a3cf0748567
Alfred-Lau <jian.liu@icloud.com>
2018-03-05 16:58:51 +0800

    [conti] number

---
+++ number.js
@@ -0,0 +1,39 @@
+// 1.1 二进制和八进制表示法
+{
+
+}
+
+// 2.1 Number.isFinite && Number.isNaN()
+{
+
+}
+
+// 3.1 Number.parseInt() && Number.parseFloat()
+{
+
+}
+
+// 4.1 Number.isInteger()
+{
+
+}
+
+// 5.1 Number.EPSILON
+{
+
+}
+
+// 6.1 安全整数和Number.isSafeInteger()
+{
+
+}
+
+// 7.1 Math对象的扩展
+{
+
+}
+
+// 8.1 指数运算符
+{
+    
+}
