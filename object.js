8d504d3a7449548fdd6b4e6d9346e7b6aebd0edd
Alfred-Lau <jian.liu@icloud.com>
2018-03-14 17:33:55 +0800

    [struct]

---
+++ object.js
@@ -0,0 +1,54 @@
+// 1.1. 属性的简洁表示法
+{
+
+}
+
+// 2.1. 属性名表达式
+{
+
+}
+
+// 3.1 方法的name属性
+{
+
+}
+
+// 4.1 Object.is()
+{
+
+}
+
+// 5.1. Object.assign()
+{
+
+}
+
+// 6.1 属性的可枚举性和遍历
+{
+
+}
+
+// 7.1 Object.getOwnPropertyDescriptors()
+{
+
+}
+
+// 8.1. __proto__属性，Object.setPrototypeOf()，Object.getPrototypeOf()
+{
+
+}
+
+// 9.1. super关键字
+{
+
+}
+
+// 10.1 Object.keys()，Object.values()，Object.entries()
+{
+    
+}
+
+// 11.1 对象的扩展运算符
+{ 
+    
+}
