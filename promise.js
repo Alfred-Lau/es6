bdcd9b215f3d9c71710015fd78965bbb00610591
Alfred-Lau <jian.liu@icloud.com>
2018-03-15 12:45:33 +0800

    [promise struct]

---
+++ promise.js
@@ -0,0 +1,54 @@
+// 1.1 Promise的含义
+{
+
+}
+
+// 2.1 基本用法
+{
+
+}
+
+// 3.1 Promise.prototype.then()
+{
+
+}
+
+// 4.1 Promise.prototype.catch()
+{
+
+}
+
+// 5.1 Promise.prototype.finally()
+{
+
+}
+
+// 6.1 Promise.all()
+{
+
+}
+
+// 7.1 Promise.race()
+{
+    
+}
+
+// 8.1 Promise.resolve()
+{
+
+}
+
+// 9.1 Promise.reject()
+{
+
+}
+
+// 10.1 应用
+{
+    
+}
+
+// 11.1 Promise.try()
+{
+    
+}
