0276622527f0bb14d92baeddcc56dcb49d65aae5
Alfred-Lau <jian.liu@icloud.com>
2018-03-15 09:56:23 +0800

    [fin]

--- array.js
+++ array.js
@@ -378,13 +378,13 @@
     arr[0].name = 'Ben';
     arr; // [{name: "Ben"}, {name: "Ben"}, {name: "Ben"}]
 
-    let arr = new Array(3).fill([]);
+    arr = new Array(3).fill([]);
     arr[0].push(5);
     arr; // [[5], [5], [5]]
 
 }
 
-// 7.1. 数组实例的entries(), keys(), values()
+// 7.1. 数组实例的entries(), keys(), values(): values方法还不被支持
 {
 
     /* 
@@ -399,9 +399,9 @@
     // 0
     // 1
 
-    for (let elem of ['a', 'b'].values()) {
-        console.log(elem);
-    }
+    // for (let elem of ['a', 'b'].values()) {
+    //     console.log(elem)
+    // }
     // 'a'
     // 'b'
 
@@ -410,7 +410,6 @@
     }
     // 0 "a"
     // 1 "b"  
-
 
     // 如果不使用for ...of循环，可以手动调用遍历器对象的next方法，进行遍历。
 
@@ -425,10 +424,141 @@
 // 8.1.数组实例的includes():返回一个布尔值，表示某个数组是否包含给定的值，和字符串的includes方法类似
 {
 
+    /* 
+  
+  该方法的第二个参数表示搜索的起始位置，默认为0。如果第二个参数为负数，则表示倒数的位置，如果这时它大于数组长度（比如第二个参数为-4，但数组长度为3），则会重置为从0开始。
+  
+  */
+
+    const val = [1, 2, 3].includes(4);
+    console.log(val);
+
+    const NanVal = [1, 2, NaN].includes(NaN); // true
+    console.log(NanVal);
+
+    // 和indexOf方法的比较：数组和字符串都有这个方法
+    /* 
+
+  indexOf方法有两个缺点，一是不够语义化，它的含义是找到参数值的第一个出现位置，所以要去比较是否不等于-1，表达起来不够直观。二是，它内部使用严格相等运算符（===）进行判断，这会导致对NaN的误判。
+
+  [NaN].indexOf(NaN)
+  // -1
+  includes使用的是不一样的判断算法，就没有这个问题。
+
+  [NaN].includes(NaN)
+  // true
+
+  */
+
+    /* 
+
+    下面代码用来检查当前环境是否支持该方法，如果不支持，部署一个简易的替代版本。
+
+    const contains = (() =>
+    Array.prototype.includes
+    ? (arr, value) => arr.includes(value)
+    : (arr, value) => arr.some(el => el === value)
+    )()
+    contains(['foo', 'bar'], 'baz'); // => false
+
+    */
+
+
+    /* 
+    另外，Map 和 Set 数据结构有一个has方法，需要注意与includes区分。
+
+    Map 结构的has方法，是用来查找键名的，比如Map.prototype.has(key)、WeakMap.prototype.has(key)、Reflect.has(target, propertyKey)。
+    Set 结构的has方法，是用来查找值的，比如Set.prototype.has(value)、WeakSet.prototype.has(value)。
+
+
+    */
 }
 
 // 9.1. 数组的空位
 
 {
+
+// 注意，空位不是undefined，一个位置的值等于undefined，依然是有值的。空位是没有任何值，in运算符可以说明这一点。
+
+    0 in[undefined,undefined,undefined]; // true
+    0 in[,,]; // false
+// 上面代码说明，第一个数组的 0 号位置是有值的，第二个数组的 0 号位置没有值。
+
+
+    /* ES5 对空位的处理，已经很不一致了，大多数情况下会忽略空位。
+
+    forEach(),
+    filter(),
+    reduce(),
+    every()和some()都会跳过空位。
+    map()会跳过空位，但会保留这个值
+    join()和toString()会将空位视为undefined，而undefined和null会被处理成空字符串。 */
+    // forEach方法
+    // [, 'a'].forEach((x, i) => console.log(i)); // 1
+
+    // // filter方法
+    // ['a',, 'b'].filter(x => true) // ['a','b']
+
+    // // every方法
+    // [, 'a'].every(x => x === 'a') // true
+
+    // // reduce方法
+    // [1,, 2].reduce((x, y) => return x + y) // 3
+
+    // // some方法
+    // [, 'a'].some(x => x !== 'a') // false
+
+    // // map方法
+    // [, 'a'].map(x => 1) // [,1]
+
+    // // join方法
+    // [, 'a', undefined, null].join('#') // "#a##"
+
+    // // toString方法
+    // [, 'a', undefined, null].toString() // ",a,,"
+    // ES6 则是明确将空位转为undefined。
+
+
+    /* 
+    .from方法会将数组的空位，转为undefined，也就是说，这个方法不会忽略空位。
+
+    Array.from(['a',, 'b'])
+    // [ "a", undefined, "b" ]
+    扩展运算符（ ...）也会将空位转为undefined。
+
+    [...['a',, 'b']]
+    // [ "a", undefined, "b" ]
+    copyWithin()会连空位一起拷贝。
+
+    [, 'a', 'b',].copyWithin(2, 0) // [,"a",,"a"]
+    fill()会将空位视为正常的数组位置。
+
+    new Array(3).fill('a') // ["a","a","a"]
+    for ...of循环也会遍历空位。
+
+    let arr = [,];
+    for (let i of arr) {
+        console.log(1);
+    }
+    // 1 1
+    上面代码中，数组arr有两个空位，for ...of并没有忽略它们。如果改成map方法遍历，空位是会跳过的。
+
+    entries()、keys()、values()、find()和findIndex()会将空位处理成undefined。
+
+    // entries()
+    [...[, 'a'].entries()] // [[0,undefined], [1,"a"]]
+
+    // keys()
+    [...[, 'a'].keys()] // [0,1]
+
+    // values()
+    [...[, 'a'].values()] // [undefined,"a"]
+
+    // find()
+    [, 'a'].find(x => true) // undefined
+
+    // findIndex()
+    [, 'a'].findIndex(x => true) // 0
+    由于空位的处理规则非常不统一，所以建议避免出现空位。 */
 
 }
