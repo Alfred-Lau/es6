b63c8ba40d3ac3b8d949da0e7dff17bcfe932f3d
Alfred-Lau <jian.liu@icloud.com>
2018-03-15 18:25:42 +0800

    [conti]

--- object.js
+++ object.js
@@ -227,8 +227,18 @@
   - 如果该参数不是对象，则会先转成对象，然后返回。
 
   - 由于undefined和null无法转成对象，所以如果它们作为参数，就会报错。
+
+  - 如果非对象参数出现在源对象的位置（ 即非首参数）， 那么处理规则有所不同。 首先， 这些参数都会转成对象， 如果无法转成对象， 就会跳过。 这意味着， 如果undefined和null不在首参数， 就不会报错。
+
   
   */
+
+    const target = {};  
+    const source01 = { name:'al'};
+    const source02 = {age:10};
+    console.log(Object.assign(target, source01, source02));
+
+    
  
   
 
