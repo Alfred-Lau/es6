3ef119b08228dbd57bf01f2b4c0a602c554533e9
潇湘 <liujian@cai-inc.com>
2018-06-12 16:17:59 +0800

    format code

--- reflect.js
+++ reflect.js
@@ -435,12 +435,12 @@
   // code core
   const queuedObservers = new Set()
 
-  const observe = fn=> queuedObservers.add(fn)
+  const observe = fn => queuedObservers.add(fn)
   const observable = obj => new Proxy(obj, { set })
   
   const set = (target, key, val, receiver) => {
     const result = Reflect.set(target, key, val, receiver)
-    queuedObservers.forEach(observe=>observe())
+    queuedObservers.forEach(observe => observe())
     return result
   }
 
@@ -452,6 +452,7 @@
 
   const print = ()=>console.log(`${person.name}, ${person.age}`)
 
+  // 订阅事件
   observe(print);
   person.name = '大神'
 
@@ -459,7 +460,7 @@
   /* 
   
   观察者模式（ Observer mode） 指的是函数自动观察数据对象， 一旦对象有变化， 函数就会自动执行。
-  
+
   上面代码中， 数据对象person是观察目标， 函数print是观察者。 一旦数据对象发生变化， print就会自动执行。
 
   下面， 使用 Proxy 写一个观察者模式的最简单实现， 即实现observable和observe这两个函数。 思路是observable函数返回一个原始对象的 Proxy 代理， 拦截赋值操作， 触发充当观察者的各个函数。
