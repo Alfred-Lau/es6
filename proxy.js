7c84095d633332bb01067f91458e7c2d47f9b316
潇湘 <liujian@cai-inc.com>
2018-06-12 09:30:18 +0800

    es6

--- proxy.js
+++ proxy.js
@@ -335,6 +335,10 @@ var assert  = require('assert')
     let handler = {
       apply:()=> 'i am the proxy'
     }
+
+    const proxy = new Proxy(target, handler)
+
+    console.log(proxy())
   }
 
 
@@ -343,12 +347,39 @@ var assert  = require('assert')
 
 // 3.1 Proxy.revocable()方法
 {
+  // Proxy.revocable方法返回一个可取消的 Proxy 实例。
+  let target = {};
+  let handler = {};
+
+  let { proxy, revoke } = Proxy.revocable(target, handler);
+
+  proxy.foo = 123;
+  proxy.foo // 123
+
+/*   revoke();
+  proxy.foo // TypeError: Revoked */
+
+  /* 
+  Proxy.revocable方法返回一个对象，该对象的 proxy 属性是Proxy实例，revoke属性是一个函数，可以取消Proxy实例。上面代码中，当执行revoke函数之后，再访问Proxy实例，就会抛出一个错误。
+
+  Proxy.revocable的一个使用场景是，目标对象不允许直接访问，必须通过代理访问，一旦访问结束，就收回代理权，不允许再次访问。
+  
+  */
     
 }
 
 // 4.1. this问题
 {
-    
+  const target = {
+    m() {
+        console.log(this === target)
+    }
+  }
+  const handler = {}
+  const proxy = new Proxy(target, handler)
+  target.m()
+  proxy.m()
+  console.log(proxy instanceof Proxy)//Proxy目前还不支持原型链，所以instanceof无效
 }
 
 // 5.1 实例：web服务的客户端
